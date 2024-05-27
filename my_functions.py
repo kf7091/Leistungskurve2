20ac1380fd90c5ade1ec57654734c9a97e017cd5
kf7091 <kf7091@mci4me.at>
2024-05-27 11:11:14 +0200

    added comments

--- my_functions.py
+++ my_functions.py
@@ -1,18 +1,19 @@
 import pandas as pd
-import plotly.graph_objects as go
 
 def read_activity_data():
     '''
-    returns a pandas dataframe with the activity data from the activity.csv file
+    returns a pandas dataframe with the activity data from the activity.csv file in working directory
     '''
 
     #load data
     activity_data = pd.read_csv('activity.csv', usecols=["Duration", "PowerOriginal"])
 
+    # create absolute time
     activity_data.at[0, "Time"] = 0
     for i in range(1, len(activity_data)):
         activity_data.at[i, "Time"] = activity_data["Duration"][i] + activity_data["Time"][i-1]
     return activity_data
+
 
 def create_powercurve(power_data: list, time: list):
     '''
@@ -20,13 +21,14 @@ def create_powercurve(power_data: list, time: list):
 
     power_data: list with the power data
     time: list with the time data
-    
-    # power_data and time must have the same length
+
+    power_data and time must have the same length
     '''
 
     power_levels = []
     durations = []
 
+    # check time for powervalues in 10W steps and save maximum value
     for power_level in range(0, int(power_data.max()), 10):
 
         start = None
@@ -48,10 +50,9 @@ def create_powercurve(power_data: list, time: list):
     return pd.DataFrame({"Duration": durations, "Power": power_levels})
 
 
+# for testing
 if __name__ == "__main__":
     activity_data = read_activity_data()
-    #print(activity_data)
+    print(activity_data)
     power_curve = create_powercurve(activity_data["PowerOriginal"], activity_data["Time"])
     print(power_curve)
-    fig = go.Figure(data=go.Scatter(x=power_curve["Duration"], y=power_curve["Power"], mode='lines'))
-    fig.show()
