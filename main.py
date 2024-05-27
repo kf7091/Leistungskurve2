36b49e7a8956df4d4b5f4adf4a5623837ea514ac
kf7091 <kf7091@mci4me.at>
2024-05-27 11:10:53 +0200

    create power curve in main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import pandas as pd
 import plotly.graph_objects as go
-from my_functions import read_activity_data
+from my_functions import read_activity_data, create_powercurve
 
 activity_data = read_activity_data()
+power_curve = create_powercurve(activity_data['PowerOriginal'], activity_data['Time'])
 
-fig = go.Figure(data=go.Scatter(x=activity_data.index, y=activity_data['PowerOriginal'], mode='lines'))
+# create figure
+fig = go.Figure(data=go.Scatter(
+    x=power_curve['Duration'],
+    y=power_curve['Power'],
+    mode='lines'
+    ))
+# add title to axis
+fig.update_layout(
+    title='Powercurve',
+    xaxis_title='Zeit / s',
+    yaxis_title='Leistung / W',
+    )
+
 fig.show()
